55add725526ce148df613857b9376f6dfa6b2bf7
beto <beto_vicio@hotmail.com>
2024-11-29 13:42:47 +0000

    added the light to the fragment shader

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -3,11 +3,33 @@ uniform vec3 uColor;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+#include ../includes/ambientLight.glsl;
+#include ../includes/directionalLight.glsl;
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
+
+    //lIGHT
+    vec3 light = vec3(0.0);
+
+    light+= ambientLight(
+        vec3(1.0), // light color
+        1.0 // light intensity
+    );
+
+    light += directionalLight(
+        vec3(1.0,1.0,1.0), // the color
+        1.0, // the intensity
+        normal, // the normal
+        vec3(1.0,1.0,1.0), // the position
+        viewDirection,
+        1.0
+    );
+
+    color*=light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
