e65e26a3be21654ad86ffe9d4cd6c3e723ba76d7
beto <beto_vicio@hotmail.com>
2024-11-29 14:40:26 +0000

    we decopled the haltone from the main fragment shader

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -7,6 +7,31 @@ varying vec3 vPosition;
 #include ../includes/ambientLight.glsl;
 #include ../includes/directionalLight.glsl;
 
+vec3 halftone(
+    vec3 color,
+    float repetition,
+    vec3 direction,
+    float low,
+    float high,
+    vec3 pointColor,
+    vec3 normal
+){
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low,high,intensity);
+    vec2 uv = gl_FragCoord.xy / uResolution.y;// this are the position of the xy viewport porsition, more the xy position of the rendered viewport 
+    uv*=repetition;
+    uv = mod(uv, 1.0);
+
+    // Disc
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5*intensity,point);
+
+    // mix colors
+    color = mix(color,pointColor,point);
+
+    return color;
+}
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
@@ -30,17 +55,28 @@ void main()
         1.0
     );
 
+
     color*=light;
 
     // Halftone 
-    float repetition = 50.0;
+    float repetition = 100.0;
+    vec3 direction = vec3(0.0,-1.0,0.0);
+    float low = -0.8;
+    float high = 1.5;
+    vec3 pointColor = vec3(1.0,0.0,0.0);
+    
 
-    vec2 uv = gl_FragCoord.xy / uResolution.y;// this are the position of the xy viewport porsition, more the xy position of the rendered viewport 
-    uv*=repetition;
-    uv = mod(uv, 1.0);
-
+    color = halftone(
+                    color,
+                    repetition,
+                    direction,
+                    low,
+                    high,
+                    pointColor,
+                    normal
+                    );
     // Final color
-    gl_FragColor = vec4(uv,1.0, 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
